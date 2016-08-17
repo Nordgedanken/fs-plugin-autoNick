455dd788fae74d304e0cba9d2a8bfba535ee03a2
MTRNord <mtrnord1@gmail.com>
2016-08-17 20:56:00 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# fs-plugin-autoNick
+A Tool to auto change the Nick to an random one
---
+++ index.js
@@ -0,0 +1,9 @@
+/* -------------------------------------- */
+// Author: MTRNord <info@nordgedanken.de>
+// Date: 17.08.2016
+// Version: 0.0.1
+/* -------------------------------------- */
+module.exports.player = function(player, serv) { // Player is a type of entity (entity inject is called first) with added properties and functions
+  player.setXp(100); // Example of a property player entities have but not other entities
+  player.on(',,,', ...);
+}
